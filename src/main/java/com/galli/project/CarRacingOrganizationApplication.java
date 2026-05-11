5284abd31cd446720ed404000142484275fc8f4f
NicGalli <nikk094@live.it>
2026-05-11 19:01:14 +0200

    Spring Boot Project created

---
+++ src/main/java/com/galli/project/CarRacingOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.galli.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRacingOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRacingOrganizationApplication.class, args);
+	}
+
+}
