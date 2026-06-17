6c7d71e637671c15529f9754fb3b07b579b63c1f
NicGalli <nikk094@live.it>
2026-06-17 12:34:36 +0200

    add testcontainer

--- src/test/java/com/galli/project/jpa/PilotJpaTest.java
+++ src/test/java/com/galli/project/jpa/PilotJpaTest.java
@@ -1,19 +1,26 @@
 package com.galli.project.jpa;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 import com.galli.project.model.Pilot;
 
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
+@Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PilotJpaTest {
+
+	@Container
+	@ServiceConnection
+	static final PostgreSQLContainer postgres = new PostgreSQLContainer(
+			"postgres:18-alpine");
 
 	@Autowired
 	private TestEntityManager entityManager;
@@ -21,12 +28,5 @@ class PilotJpaTest {
 	@Test
 	void testJpaMapping() {
 		Pilot saved = entityManager.persistFlushFind(new Pilot(null, "test"));
-//		assertThat(saved.getName()).isEqualTo("test");
-//		assertThat(saved.getSalary()).isEqualTo(1000);
-//		assertThat(saved.getId()).isNotNull();
-//		assertThat(saved.getId()).isPositive();
-//		LoggerFactory.getLogger(EmployeeJpaTest.class)
-//				.info("Saved: " + saved.toString());
 	}
-
 }
