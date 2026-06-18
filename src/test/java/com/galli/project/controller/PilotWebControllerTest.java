b2986180765e34d19bf171d3c3f1f7b395d58f93
NicGalli <nikk094@live.it>
2026-06-18 15:35:03 +0200

    add pilots page name check

--- src/test/java/com/galli/project/controller/PilotWebControllerTest.java
+++ src/test/java/com/galli/project/controller/PilotWebControllerTest.java
@@ -3,11 +3,13 @@ package com.galli.project.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
@@ -18,7 +20,16 @@ class PilotWebControllerTest {
 	private MockMvc mvc;
 
 	@Test
-	void testStatus200() throws Exception {
+	@DisplayName("Test pilots page has status 2xx")
+	void test1() throws Exception {
 		mvc.perform(get("/pilots")).andExpect(status().is2xxSuccessful());
+	}
+
+	@Test
+	@DisplayName("Test return pilots page")
+	void test2() throws Exception {
+		ModelAndViewAssert.assertViewName(
+				mvc.perform(get("/pilots")).andReturn().getModelAndView(),
+				"pilots-list");
 	}
 }
