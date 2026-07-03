94ef9f76b2a87b42dbd2f561d704c8b9d5d7e67a
NicGalli <nikk094@live.it>
2026-07-03 17:56:06 +0200

    add instructions to BDD file

--- src/bdd/java/com/galli/project/bdd/CucumberBDD.java
+++ src/bdd/java/com/galli/project/bdd/CucumberBDD.java
@@ -8,6 +8,17 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Before executing this Test Case from Eclipse run the following command from
+ * the folder where docker-compose.yml is in and wait a few seconds:
+ * 
+ * docker compose up -d postgres_db
+ * 
+ * After execution run this command from the same folder to remove the
+ * container:
+ * 
+ * docker compose down
+ */
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("features")
