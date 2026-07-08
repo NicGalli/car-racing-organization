a5d30639018b68a5d77bdfcbdb5bbbbca1cdce55
NicGalli <nikk094@live.it>
2026-07-08 17:41:56 +0200

    implement repository

---
+++ src/main/java/com/galli/project/repository/CircuitRepository.java
@@ -0,0 +1,9 @@
+package com.galli.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.galli.project.model.Circuit;
+
+public interface CircuitRepository extends JpaRepository<Circuit, Long> {
+
+}
